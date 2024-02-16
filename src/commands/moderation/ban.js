c08b35795866053bde4f5e5a9ba1e12fd0f90761
Paul Müser <pmueser@mail.uni-paderborn.de>
2024-02-16 01:20:38 +0100

    add functionality for ban.js

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,6 +1,47 @@
 const {ApplicationCommandOptionType, PermissionFlagsBits} = require('discord.js');
 
 module.exports = {
+
+    callback: async (client, interaction) => {
+        const targetUserId = interaction.options.get('target-user').value;
+        const reason = interaction.options.get('reason')?.value || "No reason provided";
+
+        await interaction.deferReply();
+
+        const targetUser = await interaction.guild.members.fetch(targetUserId);
+
+        if (!targetUser) {
+            await interaction.editReply("That user doesn't exist in this server.");
+            return;
+        }
+
+        if (targetUser.id === interaction.guild.ownerId) {
+            await interaction.editReply("You can't ban that user because they're the server owner.");
+            return;
+        }
+
+        const targetUserRolePosition = targetUser.roles.highest.position;
+        const requestUserRolePosition = interaction.member.roles.highest.position;
+        const botRolePosition = interaction.guild.members.me.roles.highest.position;
+
+        if (targetUserRolePosition >= requestUserRolePosition) {
+            await interaction.editReply("You can't ban that user because they have the same/higher role than you.");
+            return;
+        }
+
+        if (targetUserRolePosition >= botRolePosition) {
+            await interaction.editReply("I can't ban that user because they have the same/higher role than me.");
+            return;
+        }
+
+        try {
+            await targetUser.ban({reason});
+            await interaction.editReply(`User ${targetUser} was banned\nReason: ${reason}`);
+        } catch (error) {
+            console.log(`There was an error when banning: ${error}`)
+        }
+    },
+
     deleted: true,
     name: 'ban',
     description: 'Bans a member from the server.',
@@ -21,8 +62,4 @@ module.exports = {
     ],
     permissionsRequired: [PermissionFlagsBits.Administrator],
     botPermissions: [PermissionFlagsBits.Administrator],
-
-    callback: (client, interaction) => {
-        interaction.reply("ban..")
-    }
 }
