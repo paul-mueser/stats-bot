9a1db28e9ee30fd9007f3325a57255681217a961
Paul Müser <pmueser@mail.uni-paderborn.de>
2024-02-16 12:57:26 +0100

    add first draft for fetching messages

---
+++ src/utils/fetchAllMessages.js
@@ -0,0 +1,22 @@
+module.exports = async (client, channelId) => {
+    const channel = client.channels.cache.get(channelId);
+    let messages = [];
+
+    // Create message pointer
+    let message = await channel.messages
+        .fetch({ limit: 1 })
+        .then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
+
+    while (message) {
+        await channel.messages
+            .fetch({ limit: 100, before: message.id })
+            .then(messagePage => {
+                messagePage.forEach(msg => messages.push(msg));
+
+                // Update our message pointer to be the last message on the page of messages
+                message = 0 < messagePage.size ? messagePage.at(messagePage.size - 1) : null;
+            });
+    }
+
+    console.log(messages);  // Print all messages
+};
