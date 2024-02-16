7fb64358b712dc843500127f8df7021e5627451d
Paul Müser <pmueser@mail.uni-paderborn.de>
2024-02-16 15:47:07 +0100

    update basic stats message handling

--- src/handlers/quoteContentHandler.js
+++
@@ -1,6 +0,0 @@
-module.exports = (message) => {
-    let content = message.content;
-    content = content.substring(content.indexOf("~") + 1).trimStart();
-    content = content.substring(0, content.indexOf(" "));
-    return content;
-};
--- src/handlers/quoteMessageHandler.js
+++ src/handlers/quoteMessageHandler.js
@@ -1,12 +1,14 @@
-const quoteContentHandler = require('../handlers/quoteContentHandler');
-
 module.exports = (messages) => {
     const timeData = Array(24).fill(0);
     const leaderboard = new Map();
 
     for (const message of messages) {
         timeData[message.createdAt.getHours()] += 1; // update count for hour of the message
-        const name = quoteContentHandler(message);
+
+        let name = message.content;
+        name = name.substring(name.indexOf("~") + 1).trimStart();
+        name = name.substring(0, name.indexOf(" "));
+
         if (leaderboard.has(name)) {
             const oldVal = leaderboard.get(name);
             leaderboard.set(name, oldVal + 1);
