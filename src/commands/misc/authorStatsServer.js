e7e4fa0884d20e3b7cad0c10089fee8e456bdf0f
Paul Müser <pmueser@mail.uni-paderborn.de>
2024-02-27 14:44:20 +0100

    add first bit of logic for authorStatsServer

--- src/commands/misc/authorStatsServer.js
+++ src/commands/misc/authorStatsServer.js
@@ -1,12 +1,22 @@
-const {EmbedBuilder} = require('discord.js');
+const {EmbedBuilder, ApplicationCommandOptionType} = require('discord.js');
 const fetchAllMessages = require('../../utils/fetchAllMessages');
 const serverAuthorStatsHandler = require('../../handlers/serverAuthorStatsHandler');
 
 module.exports = {
     callback: async (client, interaction) => {
         let retVal = "";
+
+        let ignoredChannels = [];
+        if (interaction.options.getString('ignored channels')) {
+            ignoredChannels = interaction.options.getString('ignored channels').split(',').map(channel => channel.trim());
+        }
+
         for (const channel of interaction.guild.channels.cache) {
-            const allMessages = await fetchAllMessages(client, interaction.guild.channels.cache.find(channel => channel.name === 'zitate').id);
+            if (ignoredChannels.includes(channel[1].name)) {
+                continue;
+            }
+
+            const allMessages = await fetchAllMessages(client, channel[1].id);
 
             const messageData = await serverAuthorStatsHandler(allMessages);
 
@@ -26,4 +36,11 @@ module.exports = {
 
     name: 'authorStatsServer',
     description: 'Replies with the stats of all authors of the server.',
+    options: [
+        {
+            name: 'ignored channels',
+            description: 'List all channel names to ignore here.',
+            type: ApplicationCommandOptionType.String,
+        },
+    ],
 };
